19f468063d4855ab5e03ba641fc2033ce8c4e49d
kranthi <kranthikumarchennoji@gmail.com>
2025-04-28 12:20:04 -0600

    New URL added

--- src/test/java/tests/PostsApiTest.java
+++ src/test/java/tests/PostsApiTest.java
@@ -6,44 +6,44 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class UsersApiTest {
+public class PostsApiTest {
 
     @BeforeClass
     public void setup() {
-        RestAssured.baseURI = "https://reqres.in/";
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
     }
 
     @Test
-    public void testGetSingleUser() {
+    public void testGetSinglePost() {
         Response response = RestAssured
                 .given()
                 .when()
-                .get("/api/users/2")
+                .get("/posts/1")
                 .then()
                 .statusCode(200)
                 .extract()
                 .response();
 
         System.out.println("Response Body: " + response.asString());
-        Assert.assertEquals(response.jsonPath().getString("data.first_name"), "Janet");
+        Assert.assertEquals(response.jsonPath().getInt("id"), 1);
     }
 
     @Test
-    public void testCreateUser() {
-        String requestBody = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+    public void testCreatePost() {
+        String requestBody = "{ \"title\": \"foo\", \"body\": \"bar\", \"userId\": 1 }";
 
         Response response = RestAssured
                 .given()
                 .header("Content-Type", "application/json")
                 .body(requestBody)
                 .when()
-                .post("/api/users")
+                .post("/posts")
                 .then()
                 .statusCode(201)
                 .extract()
                 .response();
 
         System.out.println("Response Body: " + response.asString());
-        Assert.assertNotNull(response.jsonPath().getString("id"));
+        Assert.assertNotNull(response.jsonPath().getString("id"), "ID should not be null after creating post");
     }
 }
