d2249a252b5a9bced53f7a09ed9a7f3aed432d09
kranthi <kranthikumarchennoji@gmail.com>
2025-04-28 11:58:26 -0600

    Adding Test cases

--- src/test/java/tests/UsersApiTest.java
+++ src/test/java/tests/UsersApiTest.java
@@ -3,18 +3,47 @@ package tests;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class UsersApiTest {
 
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://reqres.in/";
+    }
+
     @Test
-    public void getPublicAPI() {
+    public void testGetSingleUser() {
         Response response = RestAssured
                 .given()
                 .when()
-                .get("https://jsonplaceholder.typicode.com/posts/1");
+                .get("/api/users/2")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
 
-        System.out.println("Status code: " + response.getStatusCode());
-        Assert.assertEquals(response.getStatusCode(), 200, "Status code should be 200");
+        System.out.println("Response Body: " + response.asString());
+        Assert.assertEquals(response.jsonPath().getString("data.first_name"), "Janet");
+    }
+
+    @Test
+    public void testCreateUser() {
+        String requestBody = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        Response response = RestAssured
+                .given()
+                .header("Content-Type", "application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/users")
+                .then()
+                .statusCode(201)
+                .extract()
+                .response();
+
+        System.out.println("Response Body: " + response.asString());
+        Assert.assertNotNull(response.jsonPath().getString("id"));
     }
 }
